ed471bb598f2635ceaea24c8a91623605b78974b
august <kingaugust20@icloud.com>
2024-11-18 02:59:19 -0800

    forth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 import Page from "../app/home/page";
 
-
-
 export default function Home() {
+  useEffect(() => {
+    Aos.init({
+      duration: 1500,
+      easing: "ease-in-sine",
+      delay: 100,
+      offset: 100,
+    });
+    Aos.refresh();
+  }, []);
+
   return (
     <div>
       <Page />
