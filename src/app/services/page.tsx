b98e7cc0f855b3bcff9541e604d417f60bb6c469
august <kingaugust20@icloud.com>
2024-11-18 02:51:19 -0800

    third

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import React, { useEffect } from "react";
+import React from "react";
 
 import ServiceHero from "@/app/services/serviceHero";
 import ServiceSec2 from "@/app/services/serviceSec2";
 import ServiceSec3 from "@/app/services/serviceSec3";
 
-
-
-
-import Aos from "aos";
-import "aos/dist/aos.css"
 const Navbar = dynamic(() => import("@/components/Navbar"), {
   ssr: false,
 });
@@ -21,26 +16,12 @@ const Footer = dynamic(() => import("@/components/Footer"), {
 });
 
 export default function page() {
-
-  useEffect(() => {
-    Aos.init({
-      duration: 1500,
-      easing: "ease-in-sine",
-      delay: 100,
-      offset: 100,
-    });
-    Aos.refresh();
-  }, []);
-
-
-
   return (
     <div>
       <Navbar />
       <ServiceHero />
       <ServiceSec2 />
       <ServiceSec3 />
-
 
       <Footer />
     </div>
